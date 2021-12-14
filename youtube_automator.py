15f719538f8eb0dbca21da052eeeada7f68bb9da
gabriel <grossiwm@gmail.com>
2021-12-13 22:15:05 -0300

    refactor

--- youtube_automator.py
+++ youtube_automator.py
@@ -6,26 +6,29 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-def getSecs(txt):
+def get_secs(txt):
     arr = txt.split(':')
     secs = int(arr[0])*60 + int(arr[1])
     return secs
+
+def watch(driver, url):
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+
+
+    button=wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'button.ytp-play-button')))
+    button.click()
+
+    duration_div=wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'span.ytp-time-duration')))
+    duration = get_secs(duration_div.text)
+    time.sleep(duration)
 
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 chrome=webdriver.Chrome(ChromeDriverManager().install(), options=options)
-chrome.get(URL)
-wait = WebDriverWait(chrome, 10)
 
-
-button=wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'button.ytp-play-button')))
-button.click()
-
-duration_div=wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, 'span.ytp-time-duration')))
-duration = getSecs(duration_div.text)
-print(duration)
-time.sleep(duration)
+watch(chrome, 'https://www.youtube.com/watch?v=Cf_5aLm-CGs')
 
 chrome.close()
